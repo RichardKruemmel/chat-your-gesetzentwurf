dc9dc237577f25967f1a79b751e9b3d51dd5bf0e
Richard <richard.kruemmel@code.berlin>
2023-09-01 20:49:54 +0200

    Create database schema

---
+++ database/init.sql
@@ -0,0 +1,20 @@
+DO $$ BEGIN
+  CREATE EXTENSION IF NOT EXISTS "pg_trgm";
+  IF NOT EXISTS (SELECT FROM pg_catalog.pg_tables WHERE tablename='documents') THEN
+    CREATE TABLE documents (
+      id SERIAL PRIMARY KEY,
+      name VARCHAR(255) NOT NULL,
+      date DATE NOT NULL,
+      pdf_data BYTEA NOT NULL
+    );
+  END IF;
+
+  IF NOT EXISTS (SELECT FROM pg_catalog.pg_tables WHERE tablename='embeddings') THEN
+    CREATE TABLE embeddings (
+      id SERIAL PRIMARY KEY,
+      document_id INTEGER REFERENCES documents(id),
+      text_embedding TEXT NOT NULL
+    );
+  END IF;
+END $$;
+
