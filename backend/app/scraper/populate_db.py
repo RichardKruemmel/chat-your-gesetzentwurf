6abd5027708f1d35a208d3e30250b331aff60e6d
Richard <richard.kruemmel@code.berlin>
2023-10-05 15:40:12 +0200

    Add populate function for parliaments

--- backend/app/scraper/populate_db.py
+++ backend/app/scraper/populate_db.py
@@ -1,5 +1,6 @@
 from backend.app.database.crud import insert_and_update
 from backend.app.database.models.party import Party
+from backend.app.database.models.parliament import Parliament
 from backend.app.scraper.utils import fetch_entity
 
 
@@ -15,3 +16,18 @@ def populate_parties() -> None:
         for api_party in api_parties
     ]
     insert_and_update(Party, parties)
+
+
+def populate_parliaments() -> None:
+    api_parliaments = fetch_entity("parliaments")
+    parliaments = [
+        {
+            "id": api_parliament["id"],
+            "label": api_parliament["label"],
+            "abgeordnetenwatch_url": api_parliament["abgeordnetenwatch_url"],
+            "label_external_long": api_parliament["label_external_long"],
+            "current_election": api_parliament["current_project"],
+        }
+        for api_parliament in api_parliaments
+    ]
+    insert_and_update(Parliament, parliaments)
