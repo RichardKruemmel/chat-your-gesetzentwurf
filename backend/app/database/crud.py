8b863ff205128d7cb0a19242cf90df178f584da9
Richard <richard.kruemmel@code.berlin>
2023-10-05 15:05:52 +0200

    Add type hinting and error handling

--- backend/app/database/crud.py
+++ backend/app/database/crud.py
@@ -1,23 +1,29 @@
-from typing import Any, List
+import logging
+from typing import Type, List, Dict
 
 from sqlalchemy.orm import Session
 from sqlalchemy.dialects.postgresql import insert
 
-from .database import Session
+
+from .database import Base, Session
 from app.database.schema import User
 
 
 def get_user(db: Session, username: str):
     return db.query(User).filter(User.email == username).first()
 
-
-def insert_and_update(model: Any, data: List[Any]) -> None:
+def insert_and_update(model: Type[Base], data: List[Dict]) -> None:
     session = Session()
-    stmt = insert(model).values(data)
-    stmt = stmt.on_conflict_do_update(
-        constraint=f"{model.__tablename__}_pkey",
-        set_={col.name: col for col in stmt.excluded if not col.primary_key},
-    )
-    session.execute(stmt)
-    session.commit()
-    session.close()
+    try:
+        stmt = insert(model).values(data)
+        stmt = stmt.on_conflict_do_update(
+            constraint=f"{model.__tablename__}_pkey",
+            set_={col.name: col for col in stmt.excluded if not col.primary_key},
+        )
+        session.execute(stmt)
+        session.commit()
+    except Exception as e:
+        logging.error(f"Database error: {e}")
+        session.rollback()
+    finally:
+        session.close()
