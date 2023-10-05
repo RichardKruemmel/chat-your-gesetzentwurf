ae5ce2414af7fae52f3759be118b3e480e877878
Richard <richard.kruemmel@code.berlin>
2023-10-05 19:32:25 +0200

    Run linter

--- backend/app/database/models/election.py
+++ backend/app/database/models/election.py
@@ -8,17 +8,21 @@ class Election(Base):
     __tablename__ = "election"
 
     id: Mapped[int] = mapped_column(Integer, primary_key=True)
-    label: Mapped[str] =  mapped_column(String, nullable=False)
+    label: Mapped[str] = mapped_column(String, nullable=False)
     previous_election_id: Mapped[int] = mapped_column(
         Integer, ForeignKey("election.id")
     )
-    election_date: Mapped[Date] =  mapped_column(Date, nullable=False)
-    start_date_period: Mapped[Date] =  mapped_column(Date, nullable=False)
-    end_date_period: Mapped[Date] =  mapped_column(Date, nullable=False)
-    parliament_id: Mapped[int] = mapped_column(Integer, ForeignKey("parliament.id"), nullable=False)
+    election_date: Mapped[Date] = mapped_column(Date, nullable=False)
+    start_date_period: Mapped[Date] = mapped_column(Date, nullable=False)
+    end_date_period: Mapped[Date] = mapped_column(Date, nullable=False)
+    parliament_id: Mapped[int] = mapped_column(
+        Integer, ForeignKey("parliament.id"), nullable=False
+    )
 
     parliament = relationship("Parliament", back_populates="election")
     previous_election = relationship(
         "Election", remote_side=[id], backref=backref("next_election", uselist=False)
     )
-    election_programs = relationship("ElectionProgram", back_populates="parliament_period")
+    election_programs = relationship(
+        "ElectionProgram", back_populates="election"
+    )
--- backend/app/database/models/parliament.py
+++ backend/app/database/models/parliament.py
@@ -8,9 +8,9 @@ class Parliament(Base):
     __tablename__ = "parliament"
 
     id: Mapped[int] = mapped_column(Integer, primary_key=True)
-    label: Mapped[str] =  mapped_column(String, nullable=False)
-    abgeordnetenwatch_url: Mapped[str]  =  mapped_column(String, nullable=False)
-    label_long: Mapped[str]  =  mapped_column(String, nullable=False)
+    label: Mapped[str] = mapped_column(String, nullable=False)
+    abgeordnetenwatch_url: Mapped[str] = mapped_column(String, nullable=False)
+    label_long: Mapped[str] = mapped_column(String, nullable=False)
     last_election_id: Mapped[int] = mapped_column(Integer, ForeignKey("election.id"))
 
     election = relationship("Election", back_populates="parliament")
