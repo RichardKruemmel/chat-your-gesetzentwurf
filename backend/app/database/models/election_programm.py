4f755628fd2b6bf8e817efb32cf87f9a10cefeda
Richard <richard.kruemmel@code.berlin>
2023-10-05 19:26:01 +0200

    Create election program table

---
+++ backend/app/database/models/election_programm.py
@@ -0,0 +1,19 @@
+from sqlalchemy import Integer, ForeignKey, String
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+
+from ..database import Base
+
+class ElectionProgram(Base):
+    __tablename__ = "election_program"
+
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    label: Mapped[str] =  mapped_column(String, nullable=False)
+    election_id: Mapped[int] = mapped_column(Integer, ForeignKey("election.id"), index=True, unique=True, nullable=False)
+    party_id: Mapped[int] = mapped_column(Integer, ForeignKey("party.id"), nullable=False)
+    abgeordnetenwatch_file_url: Mapped[str] =  mapped_column(String, nullable=False)
+    file_cloud_url: Mapped[str] =  mapped_column(String)
+    document_id: Mapped[int] = mapped_column(Integer, ForeignKey("documents.id"), index=True, unique=True)
+
+    election = relationship("Election", back_populates="election_programs")
+    party = relationship("Party", back_populates="election_programs")
+    document = relationship("Document", uselist=False, back_populates="election_program")
