7cc2ab17564aaea1a438d31b7cfcd99d5bd1fa58
Richard <richard.kruemmel@code.berlin>
2023-10-05 18:15:04 +0200

    Add nullable constraint, better type annotation and relationship

--- backend/app/database/models/party.py
+++ backend/app/database/models/party.py
@@ -1,11 +1,15 @@
-from sqlalchemy.orm import Mapped, mapped_column, DeclarativeBase
+from sqlalchemy import Integer, String
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+
 from ..database import Base
 
 
 class Party(Base):
     __tablename__ = "party"
 
-    id: Mapped[int] = mapped_column(primary_key=True)
-    label: Mapped[str]
-    full_name: Mapped[str]
-    short_name: Mapped[str]
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    label: Mapped[str] =  mapped_column(String, nullable=False)
+    full_name: Mapped[str] =  mapped_column(String, nullable=False)
+    short_name: Mapped[str] =  mapped_column(String, nullable=False)
+
+    election_programs = relationship("ElectionProgram", back_populates="party")
