5f77a1bf31edc8ef9635b718fd65e53baf1b9951
Richard <richard.kruemmel@code.berlin>
2023-10-05 19:22:14 +0200

    Add nullable constraint and type annotation

--- backend/app/database/models/parliament.py
+++ backend/app/database/models/parliament.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Integer, ForeignKey
+from sqlalchemy import Integer, String, ForeignKey
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 from ..database import Base
@@ -7,10 +7,10 @@ from ..database import Base
 class Parliament(Base):
     __tablename__ = "parliament"
 
-    id: Mapped[int] = mapped_column(primary_key=True)
-    label: Mapped[str]
-    abgeordnetenwatch_url: Mapped[str]
-    label_external_long: Mapped[str]
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    label: Mapped[str] =  mapped_column(String, nullable=False)
+    abgeordnetenwatch_url: Mapped[str]  =  mapped_column(String, nullable=False)
+    label_long: Mapped[str]  =  mapped_column(String, nullable=False)
     last_election_id: Mapped[int] = mapped_column(Integer, ForeignKey("election.id"))
 
     election = relationship("Election", back_populates="parliament")
