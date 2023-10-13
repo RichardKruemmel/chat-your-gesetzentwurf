80579f53c7ef73a6852a8b00e14927b02e21ec60
Richard <richard.kruemmel@code.berlin>
2023-10-13 11:58:44 +0200

    Change to consistent table naming, use consistent label column, delete embeddings relationship

--- backend/app/database/models/document.py
+++ backend/app/database/models/document.py
@@ -5,11 +5,9 @@ from ..database import Base
 
 
 class Document(Base):
-    __tablename__ = "documents"
+    __tablename__ = "document"
 
     id: Mapped[int] = mapped_column(primary_key=True, index=True)
-    name: Mapped[str] = mapped_column(String, index=True)
+    label: Mapped[str] = mapped_column(String, index=True)
     date: Mapped[Date] = mapped_column(Date)
     pdf_data: Mapped[LargeBinary] = mapped_column(LargeBinary)
-
-    embeddings = relationship("Embedding", back_populates="document", uselist=False)
