d0e8ff767fbb0d131aa2c3e0ebaa1f8d9dac8d1a
Richard <richard.kruemmel@code.berlin>
2023-10-13 12:13:05 +0200

    Add init to make models modules

---
+++ backend/app/database/models/__init__.py
@@ -0,0 +1,5 @@
+from app.database.models.party import *
+from app.database.models.parliament import *
+from app.database.models.election import *
+from app.database.models.document import *
+from app.database.models.election_programm import *
