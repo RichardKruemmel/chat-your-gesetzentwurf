b6f8e739dea154afb59272b503f22afb9ce6a621
Richard <richard.kruemmel@code.berlin>
2023-11-26 14:50:12 +0100

    Implement update function for cloud url

--- backend/app/scraper/scripts/update_db.py
+++ backend/app/scraper/scripts/update_db.py
@@ -3,7 +3,9 @@ import logging
 
 from app.scraper.utils.constants import ENTITY_MODEL_MAPPING
 from app.scraper.utils.api_utils import fetch_newest_entity_item
-from backend.app.scraper.utils.compare_and_fetch_update import compare_and_fetch_update
+from app.database.database import Session
+from app.database.models.election_programm import ElectionProgram
+from app.scraper.utils.compare_and_fetch_update import compare_and_fetch_update
 from app.database.crud import insert_and_update
 from app.database.utils.db_utils import get_last_id_from_model
 
@@ -22,3 +24,24 @@ def update_db() -> None:
         if new_data:
             insert_and_update(model, new_data)
             logging.info(f"Update in database for {entity} successful.")
+
+
+def update_file_cloud_url(s3_bucket_url: str):
+    db = Session()
+
+    try:
+        election_programs = db.query(ElectionProgram).all()
+
+        for program in election_programs:
+            new_url = f"{s3_bucket_url}/{program.election_id}/{program.id}.pdf"
+            program.file_cloud_url = new_url
+
+        db.commit()
+        logging.info("All file_cloud_urls have been updated successfully.")
+
+    except Exception as e:
+        db.rollback()
+        logging.error(f"An error occurred while updating file_cloud_urls: {e}")
+
+    finally:
+        db.close()
