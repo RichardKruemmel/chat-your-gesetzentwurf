e13cf599af75eeccf8cf31bc2734e85c8aaad093
Richard <richard.kruemmel@code.berlin>
2023-11-26 14:48:48 +0100

    Remove document table

--- backend/app/database/models/election_programm.py
+++ backend/app/database/models/election_programm.py
@@ -17,13 +17,7 @@ class ElectionProgram(Base):
     )
     abgeordnetenwatch_file_url: Mapped[str] = mapped_column(String, nullable=False)
     file_cloud_url: Mapped[str] = mapped_column(String)
-    document_id: Mapped[int] = mapped_column(
-        Integer, ForeignKey("document.id", use_alter=True), index=True
-    )
+    vector_store_id: Mapped[int] = mapped_column(Integer)
 
     election = relationship("Election", back_populates="election_programs")
     party = relationship("Party", back_populates="election_programs")
-    """ document = relationship(
-        "Document", uselist=False, back_populates="election_program", post_update=True
-    )
- """
