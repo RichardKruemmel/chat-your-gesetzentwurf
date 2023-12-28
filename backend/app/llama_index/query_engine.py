f62750bc38b8e12335edf51e44c8f5231227538f
Richard <richard.kruemmel@code.berlin>
2023-12-28 21:20:35 +0100

    Add logging to query engine setup

--- backend/app/llama_index/query_engine.py
+++ backend/app/llama_index/query_engine.py
@@ -1,6 +1,8 @@
+import logging
 from llama_index import VectorStoreIndex
 
 
 def setup_query_engine(index: VectorStoreIndex, filters: dict = None):
     query_engine = index.as_chat_engine(filters=filters)
+    logging.info("Query engine setup successfully.")
     return query_engine
