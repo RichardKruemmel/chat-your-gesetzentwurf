57e3570137f3c61290cfbd42f1c480114869ed16
Richard <richard.kruemmel@code.berlin>
2023-12-28 21:20:05 +0100

    Update OpenAI API endpoint in llm.py

--- backend/app/langchain/llm.py
+++ backend/app/langchain/llm.py
@@ -8,7 +8,7 @@ from langchain.chains import RetrievalQAWithSourcesChain, LLMChain
 
 load_dotenv()
 openai.api_type = "azure"
-openai.api_base = os.getenv("OPENAI_API_BASE")
+openai.azure_endpoint = os.getenv("OPENAI_API_BASE")
 openai.api_key = os.getenv("OPENAI_API_KEY")
 openai.api_version = os.getenv("OPENAI_API_VERSION")
 deployment_name = os.getenv("OPENAI_DEPLOYMENT_NAME")
