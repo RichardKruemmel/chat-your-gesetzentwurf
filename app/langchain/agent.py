ae78fb26a61c95d5f87d06c16ef0a168a689a008
Richard <richard.kruemmel@code.berlin>
2023-12-29 18:49:32 +0100

    Refactor langchain agent and llm module

--- app/langchain/agent.py
+++ app/langchain/agent.py
@@ -1,14 +1,14 @@
 from langchain.agents import initialize_agent
 from langchain.agents import AgentType
 
-from app.langchain.llm import chatgpt
+from app.langchain.llm import setup_chatgpt
 from app.langchain.memory import memory
 from app.llama_index.tools import setup_agent_tools
 
 
 def setup_langchain_agent():
     llama_tools = setup_agent_tools()
-
+    chatgpt = setup_chatgpt()
     langchain_agent = initialize_agent(
         llm=chatgpt,
         agent=AgentType.OPENAI_FUNCTIONS,
