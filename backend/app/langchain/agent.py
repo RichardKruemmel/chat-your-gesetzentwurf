1c820274198ba854790eccaa9da8c9d0b9a7f47e
Richard <richard.kruemmel@code.berlin>
2023-12-29 09:50:11 +0100

    Add memory support to langchain agent

--- backend/app/langchain/agent.py
+++ backend/app/langchain/agent.py
@@ -2,13 +2,18 @@ from langchain.agents import initialize_agent
 from langchain.agents import AgentType
 
 from app.langchain.llm import chatgpt
+from app.langchain.memory import memory
 from app.llama_index.tools import setup_agent_tools
 
-llama_tools = setup_agent_tools()
 
-open_ai_agent = initialize_agent(
-    llm=chatgpt,
-    agent=AgentType.OPENAI_FUNCTIONS,
-    tools=llama_tools,
-    verbose=True,
-)
+def setup_langchain_agent():
+    llama_tools = setup_agent_tools()
+
+    langchain_agent = initialize_agent(
+        llm=chatgpt,
+        agent=AgentType.OPENAI_FUNCTIONS,
+        memory=memory,
+        tools=llama_tools,
+        verbose=True,
+    )
+    return langchain_agent
